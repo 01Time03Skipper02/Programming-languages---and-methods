b31317c332b3ec4843483bedb9ae1d7e852a91fc
San4ezzZ03 <57000437+San4ezzZ03@users.noreply.github.com>
2022-04-23 11:22:26 +0300

    Add start of 10lab
    Its start of 10 lab

---
+++ C++/labs/10labIterators/cmake-build-debug/CMakeFiles/10labIterators.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "10labIterators.exe"
+  "10labIterators.exe.manifest"
+  "10labIterators.pdb"
+  "CMakeFiles/10labIterators.dir/main.cpp.obj"
+  "lib10labIterators.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/10labIterators.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C++/labs/10labIterators/main.cpp
@@ -0,0 +1,6 @@
+#include "implementation.cpp"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
