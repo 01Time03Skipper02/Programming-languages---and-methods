b2e7fc7024b94d33bc89fb557d51906af8dd3d62
San4ezzZ03 <57000437+San4ezzZ03@users.noreply.github.com>
2022-04-25 13:07:40 +0300

    Changes
    Some changes

---
+++ C++/labs/8lab patternClass/cmake-build-debug/CMakeFiles/8lab_patternClass.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/1/Desktop/GitHub/Programming-languages---and-methods/C++/labs/8lab patternClass/implemention.cpp" "C:/Users/1/Desktop/GitHub/Programming-languages---and-methods/C++/labs/8lab patternClass/cmake-build-debug/CMakeFiles/8lab_patternClass.dir/implemention.cpp.obj"
+  "C:/Users/1/Desktop/GitHub/Programming-languages---and-methods/C++/labs/8lab patternClass/main.cpp" "C:/Users/1/Desktop/GitHub/Programming-languages---and-methods/C++/labs/8lab patternClass/cmake-build-debug/CMakeFiles/8lab_patternClass.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ C++/labs/8lab patternClass/cmake-build-debug/CMakeFiles/8lab_patternClass.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "8lab_patternClass.exe"
+  "8lab_patternClass.exe.manifest"
+  "8lab_patternClass.pdb"
+  "CMakeFiles/8lab_patternClass.dir/implemention.cpp.obj"
+  "CMakeFiles/8lab_patternClass.dir/main.cpp.obj"
+  "lib8lab_patternClass.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/8lab_patternClass.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
